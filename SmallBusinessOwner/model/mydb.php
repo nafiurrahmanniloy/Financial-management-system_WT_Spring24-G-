cd01ad4b9f76c6abee98e1ed905aa3f5e639aaa3
NabilRaiyan <rraiyan77@gmail.com>
2024-02-20 09:24:13 +0600

    Creating and adding small business owner

--- SmallBusinessOwner/model/mydb.php
+++ SmallBusinessOwner/model/mydb.php
@@ -1,12 +1,15 @@
 <?php 
 
+    // Creating model class 
     class Model {
+        // creating Connection method
         function OpenConn(){
             $conn = new mysqli("localhost", "root", "", "FinancialManagementSystem");
             return $conn;
 
         }
 
+        // creating method to insert into db
         function AddSmallBusinessOwnerRegistration($conn, $table, $businessName, $businessType, $email, $password, $confirmPassword, $taxNumber, $phoneNumber){
             $insertSql = "INSERT INTO $table (businessName, businessType, email, password, confirmPassword, taxNumber, phoneNumber) VALUES ('$businessName', '$businessType', '$email', '$password', '$confirmPassword', '$taxNumber', '$phoneNumber')";
             $result = $conn->query($insertSql);
